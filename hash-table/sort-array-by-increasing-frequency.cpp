91fc49af2a0f6905c2b5bb6b4d03b58f6c3873d3
kapilchand750-lang <kapilchand750@gmail.com>
2026-05-26 08:04:58 +0530

    LC-sort-array-by-increasing-frequency [Runtime: N/A; Memory: N/A]

---
+++ hash-table/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int>map;
+        for(auto num : nums)
+            map[num]++;
+
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            if(map[a]==map[b]){
+                return a>b;
+            }
+            return map[a]<map[b];
+        });
+
+        return nums;
+    }
+};
