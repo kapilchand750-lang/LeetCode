94ee3092d51a545e80c7639a66a98271bea47604
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-01 22:04:07 +0530

    LC-print-in-order [Runtime: N/A; Memory: N/A]

---
+++ concurrency/print-in-order.cpp
@@ -0,0 +1,31 @@
+class Foo {
+private:
+    atomic<int>state;
+
+public:
+    Foo() {
+        state = 1;
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        state = 2;
+    }
+
+    void second(function<void()> printSecond) {
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        while(state<2);
+        printSecond();
+        state = 3;
+    }
+
+    void third(function<void()> printThird) {
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        while(state<3);
+        printThird();
+    }
+};
