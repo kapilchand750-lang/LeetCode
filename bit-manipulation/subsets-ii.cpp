ff43fe7b3a8239a61badfe4e148121a13dbe07e8
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-03 09:42:53 +0530

    LC-subsets-ii [Runtime: N/A; Memory: N/A]

--- bit-manipulation/subsets-ii.cpp
+++ bit-manipulation/subsets-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 private:
-    set<vector<int>>all;
+    vector<vector<int>>all;
 
-    void solve(vector<int>&nums, int n, int i, vector<int>&temp){
-        if(i==n){
-            all.insert(temp);
-            return;
+    void solve(vector<int>& nums, int idx, int n, vector<int>&temp){
+        all.push_back(temp);
+        for(int i = idx; i<n; i++){
+            if(i>idx && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            solve(nums, i+1, n, temp);
+            temp.pop_back();
         }
-
-        temp.push_back(nums[i]);
-        solve(nums, n, i+1, temp);
-        temp.pop_back();
-        solve(nums, n, i+1, temp);
     }
 
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>temp;
-        sort(nums.begin(), nums.end());
-        solve(nums, n, 0, temp);
-        vector<vector<int>>ans(all.begin(), all.end());
-        return ans;
+       vector<int>temp;
+       int n = nums.size();
+       sort(nums.begin(), nums.end());
+       solve(nums, 0, n, temp);
+       return all; 
     }
 };
