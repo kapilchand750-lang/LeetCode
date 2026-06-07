333efc7b27a539e0aa5f9b21c29b1a9c7175a180
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-07 12:06:59 +0530

    LC-permutations-ii [Runtime: N/A; Memory: N/A]

--- sorting/permutations-ii.cpp
+++ sorting/permutations-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
-    set<vector<int>>res;
+    vector<vector<int>>all;
 
-    void solve(vector<int>&nums, vector<int>&temp, int mask){
+    void solve(vector<int>&nums, int mask, vector<int>&temp){
         if(nums.size()==temp.size()){
-            res.insert(temp);
+            all.push_back(temp);
             return;
         }
 
-
         for(int i = 0; i<nums.size(); i++){
-            if((mask&(1<<i))==0){
-                temp.push_back(nums[i]);
-                solve(nums, temp, mask|(1<<i));
-                temp.pop_back();
-            }
+            if(i>0 && nums[i]==nums[i-1] && ((mask&1<<(i-1))==0)) continue;
+            if(mask&(1<<i)) continue;
+            temp.push_back(nums[i]);
+            solve(nums, mask|(1<<i), temp);
+            temp.pop_back();
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<int>temp;
-        solve(nums, temp, 0);
-        return vector<vector<int>>(res.begin(), res.end());
+        sort(nums.begin(), nums.end());
+        solve(nums, 0, temp);
+        return all;
     }
 };
