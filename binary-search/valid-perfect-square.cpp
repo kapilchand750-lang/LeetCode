eede432e87b16b48edfcb9e6076229f2bd1a9d26
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-07 16:48:08 +0530

    LC-valid-perfect-square [Runtime: N/A; Memory: N/A]

---
+++ binary-search/valid-perfect-square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int start = 0;
+        int end = num;
+        while(start<=end){
+            long long mid = start + (end - start)/2;
+            if(mid * mid > num) end = mid-1;
+            else if(mid * mid < num) start = mid + 1;
+            else return true;
+        }
+        return false;
+    }
+};
