62e3c7870b6ea4938a9add2abd5154c5a377c26b
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-10 19:30:31 +0530

    LC-combination-sum-iv [Runtime: N/A; Memory: N/A]

---
+++ array/combination-sum-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<long long>dp(target+1, 0);
+        dp[0] = 1;
+
+        for(int sum = 1; sum<=target; sum++){
+            for(auto num : nums){
+                if(sum>=num){
+                    dp[sum] += dp[sum - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
