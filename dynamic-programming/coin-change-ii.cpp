4b056f354bbd09343056c35ec6536ad5d02bb192
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-12 09:58:44 +0530

    LC-coin-change-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/coin-change-ii.cpp
+++ dynamic-programming/coin-change-ii.cpp
@@ -3,7 +3,7 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
-        vector<vector<int>>dp(n+1, vector<int>(amount+1, 0));
+        vector<vector<unsigned int>>dp(n+1, vector<unsigned int>(amount+1, 0));
         for(int i = 0; i<=n; i++) dp[i][0] = 1;
 
         for(int i = 1; i<=n; i++){
