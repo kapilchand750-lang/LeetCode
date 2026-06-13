7cf625038a8cec3f71731eae27752139611aeed4
kapilchand750-lang <kapilchand750@gmail.com>
2026-06-14 00:15:11 +0530

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- string/roman-to-integer.cpp
+++ string/roman-to-integer.cpp
@@ -1,24 +1,24 @@
 class Solution {
   public:
     int romanToInt(string s) {
-        unordered_map<char,pair<int, int>>map;
-        map['I'] = {1, 1};
-        map['V'] = {5, 2};
-        map['X'] = {10, 3};
-        map['L'] = {50, 4};
-        map['C'] = {100, 5};
-        map['D'] = {500, 6};
-        map['M'] = {1000, 7};
+        unordered_map<char,int>map;
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
 
         int n = s.size();       
         int i = n-2;
 
-        int num = map[s[n-1]].first;
+        int num = map[s[n-1]];
         while(i>=0){
            
-            if(map[s[i]].second>=map[s[i+1]].second)
-                num += map[s[i]].first;
-            else num -= map[s[i]].first;
+            if(map[s[i]]>=map[s[i+1]])
+                num += map[s[i]];
+            else num -= map[s[i]];
             i--;
         }
         return num;
