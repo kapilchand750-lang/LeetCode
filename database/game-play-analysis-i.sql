1d9fed049447d76e106001748552da728d7c8c87
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-11 22:52:44 +0530

    LC-game-play-analysis-i [Runtime: 697 ms; Memory: 0B]

--- database/game-play-analysis-i.sql
+++ database/game-play-analysis-i.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select player_id, event_date as first_login
-from Activity
-group by player_id
-having min(event_date);
+select player_id, Min(event_date) as first_login
+from Activity 
+group by player_id;
