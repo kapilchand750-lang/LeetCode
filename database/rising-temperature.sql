d79b3592a6cf28aa65567930764bf6749010a461
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-12 22:38:42 +0530

    LC-rising-temperature [Runtime: N/A; Memory: N/A]

--- database/rising-temperature.sql
+++ database/rising-temperature.sql
@@ -2,7 +2,7 @@
 select w.id
 from Weather as w
 left join Weather as k
-on w.id > k.id
+on w.id > k.id or w.id < k.id
 where 
 datediff(w.recordDate, k.recordDate) = 1 and
  k.temperature < w.temperature;
