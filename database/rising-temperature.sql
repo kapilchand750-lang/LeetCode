c569a759476a6ae86ea9e9019bf38efbd59cf02c
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-12 22:37:23 +0530

    LC-rising-temperature [Runtime: N/A; Memory: N/A]

---
+++ database/rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select w.id
+from Weather as w
+left join Weather as k
+on w.id > k.id
+where 
+datediff(w.recordDate, k.recordDate) = 1 and
+ k.temperature < w.temperature;
