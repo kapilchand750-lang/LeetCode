f6a174c9fb9132999a9ee0f3c8e9f876207ba6dc
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-17 21:58:12 +0530

    LC-sales-analysis-iii [Runtime: N/A; Memory: N/A]

---
+++ database/sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select p.product_id, p.product_name
+from Product p
+inner join Sales s
+on p.product_id = s.product_id
+group by s.product_id
+
+where s.sale_date between '2019-01-01' and '2019-03-31';
