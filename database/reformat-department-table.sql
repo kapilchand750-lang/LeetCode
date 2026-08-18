4ed55aaaacc25a1de063f7cd910ff9199c148469
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-18 21:56:11 +0530

    LC-reformat-department-table [Runtime: N/A; Memory: N/A]

---
+++ database/reformat-department-table.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select id, case(
+    when month = 'Jan' then Jan_Revenue = revenue
+    when month = 'Jan' then Jan_Revenue = revenue
+    when month = 'Jan' then Jan_Revenue = revenue
+    when month = 'Jan' then Jan_Revenue = revenue
+    when month = 'Jan' then Jan_Revenue = revenue
+    when month = 'Jan' then Jan_Revenue = revenue
+    
+)
