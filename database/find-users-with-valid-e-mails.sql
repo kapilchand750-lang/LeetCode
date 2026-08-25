1fadc73aaf3a151fa354a10c4f2dbf75e1e8578b
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-25 22:14:54 +0530

    LC-find-users-with-valid-e-mails [Runtime: N/A; Memory: N/A]

--- database/find-users-with-valid-e-mails.sql
+++ database/find-users-with-valid-e-mails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select user_id, name, mail
 from Users
-where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode.com$';
+where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$';
