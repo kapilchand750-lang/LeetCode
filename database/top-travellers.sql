da393df0ea1c13d4f8530de239c7b696393fc8e4
kapilchand750-lang <kapilchand750@gmail.com>
2026-08-26 22:23:04 +0530

    LC-top-travellers [Runtime: N/A; Memory: N/A]

--- database/top-travellers.sql
+++ database/top-travellers.sql
@@ -3,5 +3,5 @@ select name, COALESCE(SUM(distance), 0) as travelled_distance
 from Users u
 left join Rides r
 on u.id = r.user_id
-group by name
+group by user_id
 order by travelled_distance DESC, name ASC;
