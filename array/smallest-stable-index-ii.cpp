0e50c98754957ef6735433a6ee039d31c9d4b087
kapilchand750-lang <kapilchand750@gmail.com>
2026-09-05 22:05:46 +0530

    LC-smallest-stable-index-ii [Runtime: N/A; Memory: N/A]

---
+++ array/smallest-stable-index-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+
+        int instabilityScore;
+
+        int i;
+        for(i = 0; i < n; i++){
+
+            int largest = *max_element(nums.begin(), nums.begin()+i);
+
+            int smallest = *min_element(nums.begin()+i, nums.end());
+
+            instabilityScore = largest - smallest;
+
+            if(instabilityScore <= k)
+                return i;
+        }
+
+        return -1;
+    }
+};
