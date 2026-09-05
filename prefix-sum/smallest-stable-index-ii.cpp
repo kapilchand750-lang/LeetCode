bdf58f07e6e38b173ab76632394962ff340fcbfc
kapilchand750-lang <kapilchand750@gmail.com>
2026-09-05 23:08:05 +0530

    LC-smallest-stable-index-ii [Runtime: N/A; Memory: N/A]

--- prefix-sum/smallest-stable-index-ii.cpp
+++ prefix-sum/smallest-stable-index-ii.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        
         int n = nums.size();
 
+        vector<int> prefMax(n);
+        prefMax[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            if(nums[i] > prefMax[i-1])
+                prefMax[i] = nums[i];
+            else 
+            prefMax[i] = prefMax[i-1];
+        }
+
         int instabilityScore;
+        int i = 0;
+        int j = 0;
+        while(i < n){
+            auto minIndex = min_element(nums.begin()+i, nums.end());
+            int smallest = *minIndex;
+            while(nums.begin()+i <= minIndex){
+                int largest = prefMax[i];
+                
+                instabilityScore = largest - smallest;
 
-        int i;
-        for(i = 0; i < n; i++){
-
-            int largest = *max_element(nums.begin(), nums.begin()+i);
-
-            int smallest = *min_element(nums.begin()+i, nums.end());
-
-            instabilityScore = largest - smallest;
-
-            if(instabilityScore <= k)
-                return i;
+                if(instabilityScore <= k)
+                    return i;
+                
+                i++;
+            }
         }
 
         return -1;
